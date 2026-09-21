89eb7d49d072f6c79f6e5941a348aa5fdb3e0189
Nency-Bacancy <nency.vaghamshi@bacancy.com>
2026-09-21 18:51:21 +0530

    feat: implement Employee model to pass validation tests

---
+++ app/models/employee.rb
@@ -0,0 +1,13 @@
+class Employee < ApplicationRecord
+  belongs_to :department
+  belongs_to :job_title
+  belongs_to :country
+
+  enum :employment_status, { active: "active", inactive: "inactive", terminated: "terminated" }
+
+  validates :employee_number, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :joined_on, presence: true
+end
