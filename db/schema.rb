eb7d7463a853e29f7a1e8bc51fb7a32191a72250
Nency-Bacancy <nency.vaghamshi@bacancy.com>
2026-09-21 18:45:17 +0530

    feat: implement JobTitle model to pass validation tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_09_21_131230) do
+ActiveRecord::Schema[8.0].define(version: 2026_09_21_131441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -29,5 +29,13 @@ ActiveRecord::Schema[8.0].define(version: 2026_09_21_131230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_departments_on_code", unique: true
+  end
+
+  create_table "job_titles", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "code", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_job_titles_on_code", unique: true
   end
 end
