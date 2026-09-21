4394617cbffc72c8367d83a7caf8b4ece19c862e
Nency-Bacancy <nency.vaghamshi@bacancy.com>
2026-09-21 18:48:22 +0530

    test: add failing unit tests for Country validations and associations

---
+++ spec/models/country_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe Country, type: :model do
+  let(:currency) { Currency.create!(code: "USD", name: "US Dollar", symbol: "$") }
+
+  it "is valid with a name, code, and currency" do
+    country = Country.new(name: "United States", code: "US", currency: currency)
+
+    expect(country).to be_valid
+  end
+
+  it "is invalid without a name" do
+    country = Country.new(name: nil, code: "US", currency: currency)
+
+    expect(country).not_to be_valid
+    expect(country.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without a code" do
+    country = Country.new(name: "United States", code: nil, currency: currency)
+
+    expect(country).not_to be_valid
+    expect(country.errors[:code]).to include("can't be blank")
+  end
+
+  it "is invalid without a currency" do
+    country = Country.new(name: "United States", code: "US", currency: nil)
+
+    expect(country).not_to be_valid
+    expect(country.errors[:currency]).to include("must exist")
+  end
+
+  it "is invalid with a duplicate code" do
+    Country.create!(name: "United States", code: "US", currency: currency)
+    duplicate = Country.new(name: "United States of America", code: "US", currency: currency)
+
+    expect(duplicate).not_to be_valid
+    expect(duplicate.errors[:code]).to include("has already been taken")
+  end
+end
